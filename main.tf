efb73023fe6b5db34779ab2fec866b7554c95c60
Max Yakunin <yakunin.max@gmail.com>
2021-02-11 15:22:03 +0300

    Fix main.tf file

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "null_resource" "configure-dsc" {
   provisioner "remote-exec" {
 
   connection {
-    type     	  = "ssh"
-   	user     	  = "Administrator"
-    password 	  = var.local_admin_password
-  	host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
-    port          = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-   	script_path   = "/Windows/Temp/terraform_%RAND%.bat"
-   	timeout  	  = "15m"
+    type        = "ssh"
+    user        = "Administrator"
+    password    = var.local_admin_password
+    host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+    port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
+    script_path = "/Windows/Temp/terraform_%RAND%.bat"
+    timeout     = "15m"
   }
 
   inline = [data.template_file.configure-dsc.rendered]
@@ -25,127 +25,127 @@ resource "null_resource" "copy-script" {
 
   provisioner "file" {
 
-  connection {
-    type     = "ssh"
-    user     = "Administrator"
-    password = var.local_admin_password
-  	host     = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
-    port     = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-    timeout  = "15m"
-  }
+    connection {
+      type     = "ssh"
+      user     = "Administrator"
+      password = var.local_admin_password
+      host     = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+      port     = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
+      timeout  = "15m"
+    }
 
-  source = "${path.module}/dsc/adds_configuration.ps1"
-  destination = "C:/Windows/Temp/adds_configuration.ps1"
+    source      = "${path.module}/dsc/adds_configuration.ps1"
+    destination = "C:/Windows/Temp/adds_configuration.ps1"
   }
 }
 
 # Install ADDS
 resource "null_resource" "install-adds" {
-	count      = length(var.new_dc_name)
- 	depends_on = [ null_resource.copy-script ]
+  count      = length(var.new_dc_name)
+  depends_on = [ null_resource.copy-script ]
 
- 	provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-   	connection {
-   		type     	  = "ssh"
-   		user     	  = "Administrator"
-   		password 	  = var.local_admin_password
-   		host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+    connection {
+      type        = "ssh"
+      user        = "Administrator"
+      password    = var.local_admin_password
+      host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
       port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-   		script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
-   		timeout  	  = "15m"
-   	}
+      script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
+      timeout     = "15m"
+    }
 
-    inline = [data.template_file.install-adds.rendered]
- 	}
+  inline = [data.template_file.install-adds.rendered]
+  }
 }
 
 # Wait for 2 minutes
 resource "time_sleep" "wait-120-seconds" {
-  depends_on = [ null_resource.install-adds ]
+  depends_on      = [ null_resource.install-adds ]
   create_duration = "120s"
 }
 
 # Cleanup after installation
 resource "null_resource" "cleanup" {
- 	count      = length(var.new_dc_name)
- 	depends_on = [ time_sleep.wait-120-seconds ]
+  count      = length(var.new_dc_name)
+  depends_on = [ time_sleep.wait-120-seconds ]
 
- 	provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-   	connection {
-   		type     	  = "ssh"
-   		user     	  = var.deploy_mode != "NewDC" ? "Administrator" : var.existing_domain_user
-   		password 	  = var.deploy_mode != "NewDC" ? var.domain_admin_password : var.existing_domain_password
-   		host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+    connection {
+      type        = "ssh"
+      user        = var.deploy_mode != "NewDC" ? "Administrator" : var.existing_domain_user
+      password    = var.deploy_mode != "NewDC" ? var.domain_admin_password : var.existing_domain_password
+      host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
       port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-   		script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
-   		timeout  	  = "15m"
-   	}
+      script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
+      timeout     = "15m"
+    }
 
-   	inline = [data.template_file.cleanup.rendered] 
+    inline = [data.template_file.cleanup.rendered] 
   }
 }
 
 # Set DNS servers on first DC
 resource "null_resource" "first-dc-dns" {
-	count      = var.deploy_mode != "NewDC" ? length(var.new_dc_name) == 1 ? 0 : 1 : 0
-	depends_on = [ null_resource.cleanup ]
-  	
-    provisioner "remote-exec" {
+  count      = var.deploy_mode != "NewDC" ? length(var.new_dc_name) == 1 ? 0 : 1 : 0
+  depends_on = [ null_resource.cleanup ]
 
-    	connection {
-     		type     	  = "ssh"
-     		user     	  = "Administrator"
-    		password 	  = var.domain_admin_password
-   		  host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
-        port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-     		script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
-     		timeout     = "15m"
-    	}
+  provisioner "remote-exec" {
 
-     	inline = [data.template_file.first-dc-dns[0].rendered]
-	}
+    connection {
+      type        = "ssh"
+      user        = "Administrator"
+      password    = var.domain_admin_password
+      host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+      port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
+      script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
+      timeout     = "15m"
+    }
+
+    inline = [data.template_file.first-dc-dns[0].rendered]
+  }
 }
 
 # Set DNS servers on secondary DCs
 resource "null_resource" "secondary-dc-dns" {
-	count      = var.deploy_mode != "NewDC" ? length(var.new_dc_name) == 1 ? 0 : length(var.new_dc_name) - 1 : 0
-	depends_on = [ null_resource.first-dc-dns ]
+  count      = var.deploy_mode != "NewDC" ? length(var.new_dc_name) == 1 ? 0 : length(var.new_dc_name) - 1 : 0
+  depends_on = [ null_resource.first-dc-dns ]
 
- 	provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-   	connection {
-   		type        = "ssh"
-   		user        = "Administrator"
-   		password    = var.domain_admin_password
-   		host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+    connection {
+      type        = "ssh"
+      user        = "Administrator"
+      password    = var.domain_admin_password
+      host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
       port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-   		script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
-   		timeout     = "15m"
-   	}
+      script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
+      timeout     = "15m"
+    }
 
-   	inline = [data.template_file.secondary-dc-dns[0].rendered]
-	}
+    inline = [data.template_file.secondary-dc-dns[0].rendered]
+  }
 }
 
 # Set DNS servers on new DCs in existing domain
 resource "null_resource" "new-dc-dns" {
-	count      = var.deploy_mode == "NewDC" ? length(var.new_dc_name) : 0
-	depends_on = [ null_resource.cleanup ]
+  count      = var.deploy_mode == "NewDC" ? length(var.new_dc_name) : 0
+  depends_on = [ null_resource.cleanup ]
 
- 	provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-   	connection {
-   		type     	  = "ssh"
-   		user     	  = var.existing_domain_user
-   		password 	  = var.existing_domain_password
-   		host     	  = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
+    onnection {
+      type        = "ssh"
+      user        = var.existing_domain_user
+      password    = var.existing_domain_password
+      host        = var.external_ip != "" ? var.external_ip : var.internal_dc_ip[count.index]
       port        = var.external_dc_ssh_port[count.index] != "" ? var.external_dc_ssh_port[count.index] : 22
-   		script_path = "C:/Windows/Temp/terraform_%RAND%.bat"
-   		timeout  	  = "15m"
-   	}
+      script_path ="C:/Windows/Temp/terraform_%RAND%.bat"
+      timeout     = "15m"
+    }
 
     inline = [data.template_file.new-dc-dns[0].rendered]
-	}
+  }
 }
